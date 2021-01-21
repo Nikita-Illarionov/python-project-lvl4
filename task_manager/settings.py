f85d52b9e44b3220f5887ac0615f52a66ada5eb6
Nikita <Nikita.Clark@yandex.ru>
2021-01-21 13:34:46 +0300

    updating user model

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,6 +95,11 @@ DATABASES = {
 }
 
 
+
+CONN_MAX_AGE = 500
+
+db_from_env = dj_database_url.config(conn_max_age=CONN_MAX_AGE)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
