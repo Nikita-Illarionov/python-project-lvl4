9084c99cf49a21f46d70e9566d6ebbab62e71db5
Nikita <Nikita.Clark@yandex.ru>
2021-01-22 01:12:23 +0300

    urls changing

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from task_manager import views
+from users.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('', views.index),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     path('statuses/', include('statuses.urls')),
--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user_table, name='users'),
-    path('login/', views.LoginView.as_view(), name='login'),
     path('create/', views.RegisterView.as_view(), name='register'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('<int:pk>/update', views.UserUpdateView.as_view(), name='update_user'),
     path('<int:pk>/delete', views.UserDeleteView.as_view(), name='delete_user'),
 ]
